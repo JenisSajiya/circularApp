1fc6748b39997390f8bc2b28d3a45b0e3b9441e9
DSK <dsksr19@gmail.com>
2025-11-04 12:46:21 +0530

    Ready for building the app

--- frontend/app/api.js
+++ frontend/app/api.js
@@ -1,7 +1,7 @@
 // frontend/app/api.js
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const BACKEND_URL = "http://10.54.124.204:5000"; // your backend base
+export const BACKEND_URL = "http://circular-app-dzfd.vercel.app"; // your backend base
 const BASE_API = `${BACKEND_URL.replace(/\/$/, "")}/api/events`;
 const ADMIN_API = `${BACKEND_URL.replace(/\/$/, "")}/api/admin`;
 
